55e000b5deec57f59d6d93be421f66aef5752c18
Piotr Pupczyk <p.pupczyk@samsung.com>
2017-01-13 16:56:28 +0100

    init

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+
+window.onload = function () {
+    // TODO:: Do your initialization job
+
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if(e.keyName == "back")
+	try {
+	    tizen.application.getCurrentApplication().exit();
+	} catch (ignore) {
+	}
+    });
+
+    // Sample code
+    var textbox = document.querySelector('.contents');
+    textbox.addEventListener("click", function(){
+    	box = document.querySelector('#textbox');
+    	box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
+    });
+    
+};
